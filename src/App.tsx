65353d419f1c04ae3f0c1628e35b8a30ae3ca771
Alvaro <alvarofg95dev@gmail.com>
2021-05-11 19:41:16 +0200

    saga structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
+import {
+  getPendingSelector,
+  getTodosSelector,
+  getErrorSelector,
+} from "./redux/todo/selectors";
+import { fetchTodoRequest } from "./redux/todo/actions";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const pending = useSelector(getPendingSelector);
+  const todos = useSelector(getTodosSelector);
+  const error = useSelector(getErrorSelector);
+
+  useEffect(() => {
+    dispatch(fetchTodoRequest());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div style={{ padding: "15px" }}>
+      {pending ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error</div>
+      ) : (
+        todos.map((todo, index) => (
+          <div style={{ marginBottom: "10px" }} key={todo.id}>
+            {++index}. {todo.title}
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
